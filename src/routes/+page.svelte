2628da85527e529f056b99bc7854b071562a3f32
Josef Eklund <bjorn.josef.eklund@gmail.com>
2023-07-24 20:18:12 +0200

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+
+
+
+
+<div class="w-screen h-screen bg-[#fff5bb]">
+
+
+	<div class="w-full h-full">
+
+		<div class="relative top-0 left-0">
+
+			<img src="https://media.discordapp.net/attachments/964965671266033705/1129792272142180352/image.png?width=1219&height=670" draggable="false" class="border-black border-2 blur-sm w-screen h-screen absolute top-0 left-0" alt="">
+			<div class="absolute w-screen flex items-center flex-col h-screen">
+				<div class="flex flex-col items-center w-screen">
+					
+					<img src="https://dunb17ur4ymx4.cloudfront.net/webstore/logos/c5317a431d0ea52989d8d31ea3b8b63069a8a38b.png" draggable="false" class="top-0 absolute" alt="">
+					<img src="https://media.discordapp.net/attachments/866643267943792660/1133062130103484506/COBBLESTONE-7-24-20232.png?width=1200&height=283" draggable="false" class="absolute top-44" alt="">
+					
+				</div>
+				<div class="h-4/6">
+
+				</div>
+				<div class="flex flex-col justify-center items-center h-full">
+					<div class="flex flex-row">
+						<a target="_blank" href="https://store.cobblestone.gg" class="px-4 text-4xl py-2 w-full font-extrabold rounded-3xl transition-all">
+							<img src="https://media.discordapp.net/attachments/866643267943792660/1133093906985275432/STORE-7-24-2023.png?width=885&height=348" class="scale-90" draggable="false" alt="">
+						</a>
+					</div>
+					<div class="flex flex-row">
+						<a target="_blank" href="http://discord.cobblestone.gg" class="px-4 text-4xl py-2 w-full font-extrabold rounded-3xl transition-all">
+							<img src="https://media.discordapp.net/attachments/866643267943792660/1133094361756864522/discord-7-24-2023.png?width=1200&height=375" class="scale-90" draggable="false" alt="">
+						</a>
+					</div>
+				</div>
+				<!-- svelte-ignore a11y-click-events-have-key-events -->
+				<img src="https://media.discordapp.net/attachments/866643267943792660/1133094900326486056/play-cobblestone-gg-7-24-2023.png?width=1200&height=219" on:click={()=>copy()} alt="">
+			</div>
+			
+		</div>
+	</div>
+
+</div>
+<script>
+	function copy() {
+      navigator.clipboard.writeText("play.cobblestone.gg");
+    } 
+</script>
